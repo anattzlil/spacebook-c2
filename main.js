a3bb3cc1e11e493ab7836ce3781d2c5207edd2b7
anattzlil <anattzlil@gmail.com>
2017-12-27 18:21:05 +0200

    comments and fixing array

--- main.js
+++ main.js
@@ -10,41 +10,79 @@ var postId = 0;
 var addPost = function(input, id) {
     var newPost = {
         userInput: input,
-        userId: id
+        userId: id,
+        comments:[]
     }
-    console.log(newPost);
     posts.push(newPost);
 }
 //render post to the html+remove button
 var renderPost = function() {
     $('.posts').find('p').remove();
     for( var i = 0; i < posts.length; i++){
-       $('.posts').append('<p class="post">' + posts[i].userInput + '</p>');
+       $('.posts').append('<p class="post" data-index=' + i + '>' + posts[i].userInput + '</p>');
        $('p:last-child').attr('data-id', posts[i].userId);
        $('p:last-child').append('<button type="button" class="btn btn-primary remove">Remove</button>');
-       $('.remove:last-child').on('click', removePost);
+       $('p:last-child').append('<div class="comments"></div><form class="comment-form"><h4>Add a Comment</h4><div class="form-group"><input type="text" class="form-control username" placeholder="Username"><input type="text" class="form-control comment-name" placeholder="Comment Text"></div><button class="btn btn-primary add-comment" type="button">Post</button></form>');
+       for(var j = 0; j < posts[i].comments.length; j++ ){
+            $('.comments').append('<p><b>' + posts[i].comments[j].name + ' says:</b></p><p class="post-comment" data-index=' + j + '>' + posts[i].comments[j].text + '</p>');
+       }
     }
 }
+
+
 
 
 //remove post and button
 var removePost = function(){
     var itemtoRemove = $(this).closest('p');
     console.log(itemtoRemove);
-    posts.splice($.inArray(itemtoRemove.data().id, posts),1);
-    itemtoRemove.remove();
+    posts.splice(itemtoRemove.data().index,1);
+    renderPost();
 }
 
 //taking info from html
 var addItem = function(){
     var postContent = $('#post-name');
-    postId +=1
+    postId +=1;
     addPost(postContent.val(), postId);
     postContent.val("");
 }
 
+//taking comment from html
+var inputComment = function() {
+    var userName = $('.username').val();
+    var comment = $('.comment-name').val();
+    addComment(userName, comment);
+    // userName.val("");
+    // comment.val("");
+}
+
+var addComment = function(username, comment) {
+    var newComment = {
+        name: username,
+        text: comment
+    }
+    var indexNumber = $('.comment-name').closest('p');
+    console.log(indexNumber);
+    posts[indexNumber.data().index].comments.push(newComment);
+}
+
+
+//click-add post
 $('.add-post').on('click', function() {
     addItem();
     renderPost();
 });
 
+//click-remove post
+$('.posts').on('click', '.remove', removePost);
+
+
+//click-add comment
+$(document).on('click', '.add-comment', function (){
+    inputComment();
+    console.log(posts);
+    renderPost();
+});
+
+console.log(posts);
